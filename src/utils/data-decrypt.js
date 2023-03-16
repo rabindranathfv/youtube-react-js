bc81dd8e83a808a2fb92a8f601fff1e6ed9a637b
Rabindranath Ferreira <ferreiralox@gmail.com>
2023-03-16 22:38:15 +0100

    add try catch for handle Error

--- src/utils/data-decrypt.js
+++ src/utils/data-decrypt.js
@@ -2,6 +2,10 @@ import CryptoJS from "crypto-js";
 import { secretKey } from "../constants/secret-key";
 
 export const dataDecrypt = (value) => {
-  const bytes = CryptoJS.AES.decrypt(value, secretKey);
-  return JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
+  try {
+    const bytes = CryptoJS.AES.decrypt(value, secretKey);
+    return JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
+  } catch (error) {
+    console.log("🚀 ~ file: data-decrypt.js:9 ~ dataDecrypt ~ error:", error);
+  }
 };
--- src/utils/data-encrypt.js
+++ src/utils/data-encrypt.js
@@ -2,5 +2,9 @@ import CryptoJS from "crypto-js";
 import { secretKey } from "../constants/secret-key";
 
 export const dataEncrpt = (value) => {
-  return CryptoJS.AES.encrypt(JSON.stringify(value), secretKey).toString();
+  try {
+    return CryptoJS.AES.encrypt(JSON.stringify(value), secretKey).toString();
+  } catch (error) {
+    console.log("🚀 ~ file: data-encrypt.js:9 ~ dataEncrpt ~ error:", error);
+  }
 };
